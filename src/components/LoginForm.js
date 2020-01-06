57685a4bd65eda13eb2ba8ee9d43d103389bab80
Vincent Sanders <vincesanders@hotmail.com>
2020-01-06 16:29:00 -0600

    Added functionality to switch between register form and login form.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,25 +4,37 @@ import axios from 'axios';
 export default () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [retypedPassword, setRetypedPassword] = useState('');
+    const [isNewUser, setIsNewUser] = useState(false);
 
-    function handleSubmit() {
-        const values = {
-            username: username,
-            password: password
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (isNewUser) {
+
+        } else {
+            const values = {
+                username: username,
+                password: password
+            }
+    
+            // axios
+            //     .post('/api/auth/login/', values)
+            //     .then(res => {
+            //         console.log(res);
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     });
         }
-
-        axios
-            .post('/api/auth/login/', values)
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.log(err);
-            });
     }
 
     function handleNewUser() {
-
+        if (isNewUser) {
+            setIsNewUser(false);
+        } else {
+            setIsNewUser(true);
+        }
     }
 
     function handleUsernameChange(e) {
@@ -33,14 +45,36 @@ export default () => {
         setPassword(e.target.value);
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h3>Login</h3>
-            <input type='text' placeholder='Username' onChange={handleUsernameChange} />
-            <input type='password' placeholder='Password' onChange={handlePasswordChange} />
-            <input type='submit' value='Login' />
-            <p>Don't have an account?</p>
-            <button onClick={handleNewUser}>Create One</button>
-        </form>
-    );
+    function handleRetypedPassword(e) {
+        setRetypedPassword(e.target.value);
+    }
+
+    function displayedForm() {
+        if (isNewUser) {
+            return (
+                <form>
+                   <h3>Create a new account</h3> 
+                   <h3>Login</h3>
+                    <input type='text' name='username' placeholder='Username' onChange={handleUsernameChange} />
+                    <input type='password' name='password' placeholder='Password' onChange={handlePasswordChange} />
+                    <input type='password' name='retypedPassword' placeholder='Re-type Password' onChange={handleRetypedPassword} />
+                    <input type='submit' value='Login' />
+                    <p>Already have an account?</p>
+                    <button onClick={handleNewUser}>Login</button>
+                </form>
+            );
+        }
+        return (
+            <form onSubmit={handleSubmit}>
+                <h3>Login</h3>
+                <input type='text' name='username' placeholder='Username' onChange={handleUsernameChange} />
+                <input type='password' name='password' placeholder='Password' onChange={handlePasswordChange} />
+                <input type='submit' value='Login' />
+                <p>Don't have an account?</p>
+                <button onClick={handleNewUser}>Create One</button>
+            </form>
+        );
+    }
+
+    return displayedForm();
 }
