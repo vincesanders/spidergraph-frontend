f9c7161baa5454f40219e2113dbda69bc7a6a844
Vincent Sanders <vincesanders@hotmail.com>
2020-01-06 15:40:38 -0600

    Added axios dependency. Added functions to form buttons.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 export default () => {
+
+    function handleSubmit() {
+
+    }
+
+    function handleNewUser() {
+
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h3>Login</h3>
             <input type='text' placeholder='Username' />
             <input type='password' placeholder='Password' />
             <input type='submit' value='Login' />
+            <p>Don't have an account?</p>
+            <button onClick={handleNewUser}>Create One</button>
         </form>
     );
 }
