e51e388c084d54d2bf1ef591109683ff3e081cfd
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2020-01-07 14:06:09 -0800

    made title edittable, connected to store

---
+++ src/components/Dashboard/TitleInput.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import {useSelector, useDispatch} from 'react-redux';
+import actions from '../../states/spider-graph/actions';
+
+const TitleInput = styled.input`
+    font-size: 48px;
+    font-weight: 600;
+
+    background: none;
+    border: none;
+    display: inline-block;
+
+    margin: 10px 0;
+
+    &:hover{
+        background: white;
+        box-shadow: inset 0px 0px 0px 1px grey;
+    }
+
+    &:focus{
+        background: white;
+        box-shadow: inset 0px 0px 0px 1px grey;
+    }
+`
+
+export default () => {
+    const dispatch = useDispatch();
+    const title = useSelector(state => state.spiders[state.openedSpider].title);
+
+    const handleChange = e => {
+        dispatch({type: actions.EDIT_GRAPH_TITLE, payload: e.target.value})
+    }
+
+    return(
+        <TitleInput value={title} onChange={handleChange}/>
+    )
+}
