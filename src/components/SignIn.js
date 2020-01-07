874080a1969bbfb180303110791e20bb60383172
Patrick Roach <patrickevanroach@gmail.com>
2020-01-07 13:30:34 -0700

    Styling updated to match UX design

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,62 @@
 /// external modules ///
 import React from 'react'
+import styled from 'styled-components'
 
 import SignInForm from 'components/AuthForms/SignInForm'
 import { client } from 'routes'
+
+
+const SignInCont = styled.div`
+  background: #FFFFFF;
+  height: 100vh;
+  display:flex;
+  justify-content: center;
+  margin: 5px;
+`
+
+const H3 = styled.h3`
+position: absolute;
+width: 684px;
+height: 51px;
+margin: 20% 9px;
+
+/* H3 Text */
+
+font-family: Open Sans;
+font-style: normal;
+font-weight: 600;
+font-size: 48px;
+line-height: 167%;
+
+text-align: center;
+
+color: #4054B2;`
+
+const Formcont = styled.div`
+width: 100%;
+margin: 10px 20px;
+display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+	align-content: stretch;
+`
+
+const Logo = styled.button`
+
+position: absolute;
+left:20%;
+top: 20%
+
+border:none;
+background-color: #FFFFFF;
+color:#4054B2;
+font-family: Open Sans;
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 38px;`
 
 /***************************************
   MAIN
@@ -13,12 +67,16 @@ const SignIn = (props) => {
   }
 
   return (
-    <div className='SignIn'>
-      SignIn
+    <SignInCont>
+      <H3>
+       Log In
+      </H3>
+      <Formcont>
       <SignInForm />
-      <p>Don't have an account?</p>
-      <button onClick={routeToSignUp}>Create One</button>
-    </div>
+      <Logo onClick={routeToSignUp}>Spider.Graph</Logo>
+      </Formcont>
+    </SignInCont>
+    
   )
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,61 @@
 /// external modules ///
 import React from 'react'
+import styled from 'styled-components'
 
 import SignUpForm from 'components/AuthForms/SignUpForm'
 import { client } from 'routes'
+
+const SignUpCont = styled.div`
+  background: #FFFFFF;
+  height: 100vh;
+  display:flex;
+  justify-content: center;
+  margin: 5px;
+`
+
+const H3 = styled.h3`
+position: absolute;
+width: 684px;
+height: 51px;
+margin: 20% 9px;
+
+/* H3 Text */
+
+font-family: Open Sans;
+font-style: normal;
+font-weight: 600;
+font-size: 48px;
+line-height: 167%;
+
+text-align: center;
+
+color: #4054B2;`
+
+const Formcont = styled.div`
+width: 100%;
+margin: 10px 20px;
+display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+	align-content: stretch;
+`
+
+const Logo = styled.button`
+
+position: absolute;
+left:20%;
+top: 20%
+
+border:none;
+background-color: #FFFFFF;
+color:#4054B2;
+font-family: Open Sans;
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 38px;`
 
 /***************************************
   MAIN
@@ -13,12 +66,14 @@ const SignUp = (props) => {
   }
 
   return (
-    <div className='SignUp'>
-      SignUp
+    <SignUpCont>
+      <H3>Create an Account</H3>
+      <Formcont>
       <SignUpForm />
-      <p>Already have an account?</p>
-      <button onClick={routeToSignIn}>Login</button>
-    </div>
+      <Logo onClick={routeToSignIn}>Spider.Graph</Logo>
+      </Formcont>
+    </SignUpCont>
+    
   )
 }
 
