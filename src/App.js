c0431ede969da89038bc536147268c6facaf42a4
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-07 12:13:31 -0500

    update imports

--- src/App.js
+++ src/App.js
@@ -12,14 +12,18 @@ import {
 import {
   PrivateRoute,
   Home,
-  Users,
+  // Users,
   User,
-  Graphs,
+  // Graphs,
   Graph,
   SignUp,
   SignIn,
   SignOut,
 } from 'components'
+import {
+  Users,
+  Graphs,
+} from 'components/_stretch_'
 
 /// routes ///
 import { client } from 'routes'
