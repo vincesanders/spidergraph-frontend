e6e4f6aecdf84ac734e7af71b10ab43971db3134
Vincent Sanders <vincesanders@hotmail.com>
2020-01-07 15:12:55 -0600

    Added functionality for adding new categories to the table.

--- src/components/Dashboard/DataTable.js
+++ src/components/Dashboard/DataTable.js
@@ -9,30 +9,43 @@ const DataTable = () => {
         ['Category4', 5],
         ['Category5', 5]
     ]);
+
+    function addNewCategory() {
+        let newCategory = ['Category'];
+        data[0].forEach((col, i) => {
+            if (i > 0) {
+                newCategory.push(5);
+            }
+        })
+        setData(data => [...data, newCategory]);
+    }
     
     return (
-        <table>
-            <thead>
-                <tr>
-                    {data[0].map((item, i) => {
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        {data[0].map((item, i) => {
+                            return (
+                                <th key={i+1}>{item}</th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                {data.map((arr, i) => {
+                    if (i > 0) {
                         return (
-                            <th key={i+1}>{item}</th>
+                            <tr key={i+1}>
+                                {arr.map((cell, j) => (<td key={(i+1) * j}>{cell}</td>))}
+                            </tr>
                         );
-                    })}
-                </tr>
-            </thead>
-            <tbody>
-            {data.map((arr, i) => {
-                if (i > 0) {
-                    return (
-                        <tr key={i+1}>
-                            {arr.map((cell, j) => (<td key={(i+1) * j}>{cell}</td>))}
-                        </tr>
-                    );
-                }
-            })}
-            </tbody>
-        </table>
+                    }
+                })}
+                </tbody>
+            </table>
+            <button onClick={addNewCategory}>+ New Category</button>
+        </div>
     );
 }
 
