d3742333fc9da92481824f6673286a044af6fa25
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-09 17:30:11 -0500

    make Dash like Home

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,13 +1,23 @@
 /// external modules ///
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 /// internal modules ///
 import Dashboard from './Dashboard';
+import { getUserGraphs } from 'states/spider-graph/thunks'
+import { user } from 'tools/auth'
 
 /***************************************
   MAIN
 ***************************************/
 const Dash = (props) => {
+  const currentUser = user.data.get ()
+  const dispatch = useDispatch ()
+
+  useEffect (() => {
+    dispatch (getUserGraphs (currentUser.id))
+  }, [currentUser, dispatch])
+
   return (
     <div className='Dash'>
       <Dashboard />
