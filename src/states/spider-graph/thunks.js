3276bf28778e1c3c333d0e165402c41bc38da841
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-09 12:04:08 -0500

    setup thunks

---
+++ src/states/spider-graph/thunks.js
@@ -0,0 +1,83 @@
+// import act from 'states/act'
+
+/***************************************
+  MAIN
+***************************************/
+
+/// auth ///
+
+export const signUp = () => () => {
+
+}
+
+export const signIn = () => () => {
+
+}
+
+export const signOut = () => () => {
+
+}
+
+/// all users -- stretch ///
+
+export const getUsers = () => () => {
+
+}
+
+/// all graphs -- stretch ///
+
+export const getGraphs = () => () => {
+
+}
+
+/// user ///
+
+export const getUser = () => () => {
+
+}
+
+/// user graphs ///
+
+export const getUserGraphs = () => () => {
+
+}
+
+/// graph ///
+
+export const postGraph = () => () => {
+
+}
+
+export const getGraph = () => () => {
+
+}
+
+export const putGraph = () => () => {
+
+}
+
+export const deleteGraph = () => () => {
+
+}
+
+/**************************************/
+
+export default {
+  /// auth ///
+  signUp,
+  signIn,
+  signOut,
+  /// all users -- stretch ///
+  getUsers,
+  /// all graphs -- stretch ///
+  getGraphs,
+  /// user ///
+  getUser,
+  /// user graphs ///
+  getUserGraphs,
+  /// graph ///
+  postGraph,
+  getGraph,
+  putGraph,
+  deleteGraph,
+}
