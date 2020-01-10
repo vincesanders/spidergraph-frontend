7b43817c2d67f23fb7132336cc51f70942f109b2
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-09 19:29:27 -0500

    create SignOutForm

---
+++ src/components/AuthForms/SignOutForm.js
@@ -0,0 +1,15 @@
+/// external modules ///
+import React from 'react'
+
+/***************************************
+  MAIN
+***************************************/
+const SignOutForm = (props) => {
+  return (
+    <div className='SignOutForm'>SignOutForm</div>
+  )
+}
+
+/**************************************/
+
+export default SignOutForm
