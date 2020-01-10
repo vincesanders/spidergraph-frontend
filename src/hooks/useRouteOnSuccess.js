34da4e4057c40b97312cf5da12755cc37d509640
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-10 10:45:21 -0500

    simplify args

--- src/hooks/useRouteOnSuccess.js
+++ src/hooks/useRouteOnSuccess.js
@@ -4,13 +4,14 @@ import { useEffect } from 'react'
   MAIN
 ***************************************/
 
-const useRouteOnSuccess = (name, routeTo, status, ...moreDeps) => {
+const useRouteOnSuccess = (name, routeTo, deps) => {
+  const status = deps[0]
   useEffect (() => {
     console.log (`--- ${name} : ${status} ---`)
     if (status === 'success') {
       routeTo ()
     }
-  }, [status, ...moreDeps])
+  }, deps)
 }
 
 /**************************************/
